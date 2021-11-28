9f9a6e3dbaec6fa0aa9d0bfc1883e8bd8d923e3e
eruj22 <prci.prnaver2@gmail.com>
2021-11-28 08:47:30 +0100

    responsive nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAppContext } from "../context/app_context";
 import { NavLink, Link } from "react-router-dom";
 import logo from "../assets/viberate-color.svg";
@@ -7,6 +7,8 @@ import { BiSearch } from "react-icons/bi";
 
 function Navigation() {
   const { allArtists } = useAppContext();
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
 
   return (
     <nav className="nav">
@@ -14,7 +16,7 @@ function Navigation() {
         <img src={logo} className="nav__logo" alt="viberate red logo" />
       </Link>
 
-      <ul className="nav__links">
+      <ul className={`nav__links ${isNavOpen ? "nav--open" : ""}`}>
         {allArtists.map((artist) => {
           const { artist_name, artist_uuid } = artist;
 
@@ -23,8 +25,9 @@ function Navigation() {
               <NavLink
                 to={`/${artist_uuid}`}
                 className={({ isActive }) =>
-                  isActive ? "nav--active" : "nav__link"
+                  isActive ? "nav__link nav--active" : "nav__link"
                 }
+                onClick={() => setIsNavOpen(false)}
               >
                 {artist_name}
               </NavLink>
@@ -34,8 +37,19 @@ function Navigation() {
       </ul>
 
       <div className="nav__rightMenu">
-        <BiSearch className="nav__search" />
-        <GiHamburgerMenu className="nav__hamburger" />
+        <input
+          type="search"
+          className={`nav__search ${showSearch ? "show" : ""}`}
+          placeholder="Search artists"
+        />
+        <BiSearch
+          className="nav__searchBtn"
+          onClick={() => setShowSearch(!showSearch)}
+        />
+        <GiHamburgerMenu
+          className="nav__hamburger"
+          onClick={() => setIsNavOpen(!isNavOpen)}
+        />
       </div>
     </nav>
   );
