e658f894ae823cf06eabb93e4284281faf1bc78a
eruj22 <prci.prnaver2@gmail.com>
2021-11-29 14:38:13 +0100

    added hero page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Loader from "../components/Loader";
 import { useAppContext } from "../context/app_context";
+import hero from "../assets/hero.jpg";
+import heroMobile from "../assets/hero_mobile.jpg";
 
 function Home() {
   const { areArtistsLoading } = useAppContext();
@@ -10,9 +12,15 @@ function Home() {
   }
 
   return (
-    <div>
-      <h1>home</h1>
-    </div>
+    <section className="hero">
+      <div className="hero__text">
+        <h1 className="hero__title">Viberate</h1>
+        <p className="hero__subtitle">Music analytics for professionals</p>
+      </div>
+
+      <img src={hero} alt="" className="hero__image imageDesktop" />
+      <img src={heroMobile} alt="" className="hero__image imageMobile" />
+    </section>
   );
 }
 
